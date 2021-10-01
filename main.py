b003990552356040bae5fac2571e2643794fcde8
atopcu21 <91716267+atopcu21@users.noreply.github.com>
2021-10-01 06:29:21 +0000

    ver01

---
+++ main.py
@@ -0,0 +1,6 @@
+import math
+x_value = int(input("enter number x: "))
+y_value = int(input("enter number y: "))
+print("x**y =", x_value**y_value)
+print("log(x) =", math.log2(x_value))
+print("student id = 79343")
